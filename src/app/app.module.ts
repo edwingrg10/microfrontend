b7e184e7920102c41cf8a7372dd8fe96e0c7dd39
edwingrg10 <edwin.inova@gmail.com>
2024-02-13 11:21:13 -0500

    fix dependies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CursosRoutingModule } from 'projects/cursos/src/app/app-routing.module';
 import { LayoutComponent } from './shared/compontents/layout/layout.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MaterialModule } from './shared/material/material.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -22,8 +20,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     BrowserAnimationsModule,
     CommonModule,
     FormsModule,
-    MaterialModule,
-    FlexLayoutModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
